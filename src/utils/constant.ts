3012dbefa72aea71bd96c8b06ff83cc642456123
IlhamLamp <ilhamutomo24@gmail.com>
2024-05-31 22:05:16 +0700

    fix: const fe url

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -3,4 +3,4 @@ export const MIDTRANS_CLIENT_KEY = process.env.MIDTRANS_CLIENT_KEY;
 export const MIDTRANS_APP_URL = process.env.MIDTRANS_APP_URL;
 export const MIDTRANS_API_URL = process.env.MIDTRANS_API_URL;
 export const MIDTRANS_AUTH_STRING = btoa(MIDTRANS_SERVER_KEY);
-export const FRONT_END_URL = 'https://foodine-beta.vercel.app/menu';
+export const FRONT_END_URL = 'https://foodine-beta.vercel.app';
