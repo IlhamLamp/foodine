1e76cdece9d8359eb22faf4f33943414e04f9156
IlhamLamp <ilhamutomo24@gmail.com>
2024-08-07 21:25:38 +0700

    cron test file

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server"
 
-export function GET(req: Request) {
-    const result = "Hello, this is cron"
-    return NextResponse.json({ data: result })
+export function GET() {
+    const result = "Hello, this is cron";
+    return NextResponse.json({ msg: result }, {status: 200});
 }
